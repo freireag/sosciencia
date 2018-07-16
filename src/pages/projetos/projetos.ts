bce558bde55b309f42ee731e1ef4f02d5812f205
Thiago Freire <freireag@gmail.com>
2018-07-16 01:25:23 -0300

    Creating the Projetos list

--- src/pages/projetos/projetos.ts
+++ src/pages/projetos/projetos.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-projetos',
   templateUrl: 'projetos.html'
 })
 export class ProjetosPage {
+  projetos:any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http) {
 
+  }
+
+  ionViewDidLoad() {
+    this.loadItems();
+  }
+
+  loadItems() {
+    this.http.get('assets/data/projetos.json').map(res => res.json()).subscribe(data => {
+      this.projetos = data.projetos;
+    })
   }
 
 }

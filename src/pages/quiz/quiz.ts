b851038e48a107f1b4e54567d3d8ca51708c7925
Thiago Freire <freireag@gmail.com>
2018-07-22 23:47:02 -0300

    Style changes

--- src/pages/quiz/quiz.ts
+++ src/pages/quiz/quiz.ts
@@ -18,8 +18,13 @@ export class QuizPage {
     
   }
 
+  // TODO: translate to Angular code. See Orcamento page
   ionViewDidLoad() {
     (function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src='https://embed.playbuzz.com/sdk.js';fjs.parentNode.insertBefore(js,fjs);}(document,'script','playbuzz-sdk'));
+  }
+
+  openSobre() {
+    this.navCtrl.push('SobrePage');
   }
 
 }
